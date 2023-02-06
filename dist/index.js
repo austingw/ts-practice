249bd9008fe6abaa029325c7d6206e6f3673e61a
austingw <senortinman@gmail.com>
2023-02-05 23:16:52 -0500

    Playing around with the document type and lib config

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,3 @@
 "use strict";
-console.log("test");
+const btn = document.getElementById("btn");
+console.log(btn);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
-console.log("test");
+const btn = document.getElementById("btn");
+console.log(btn);
