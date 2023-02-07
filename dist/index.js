e642567e5fc020138d3901dc43ea2e401a37d3e7
austingw <senortinman@gmail.com>
2023-02-07 10:13:14 -0500

    Add todo list functionality

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,17 @@
 "use strict";
 const btn = document.getElementById("btn");
 const input = document.getElementById("todoinput");
-btn.addEventListener("click", function () {
-    alert(input.value);
+const form = document.querySelector("form");
+const list = document.getElementById("todolist");
+function handleSubmit(e) {
+    e.preventDefault();
+    const newTodo = input.value;
+    const newLI = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    newLI.append(newTodo);
+    newLI.append(checkbox);
+    list === null || list === void 0 ? void 0 : list.append(newLI);
     input.value = "";
-});
+}
+form.addEventListener("submit", handleSubmit);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 const btn = document.getElementById("btn")!;
 const input = document.getElementById("todoinput")! as HTMLInputElement;
+const form = document.querySelector("form")!;
+const list = document.getElementById("todolist")!;
 
-btn.addEventListener("click", function () {
-  alert(input.value);
+function handleSubmit(e: SubmitEvent) {
+  e.preventDefault();
+  const newTodo = input.value;
+  const newLI = document.createElement("li");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  newLI.append(newTodo);
+  newLI.append(checkbox);
+  list?.append(newLI);
+
   input.value = "";
-});
+}
+
+form.addEventListener("submit", handleSubmit);
